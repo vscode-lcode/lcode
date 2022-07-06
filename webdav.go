e4ff9529ad2b8282684f4f9a05dea402265ed76b
shynome <shynome@gmail.com>
2022-07-07 06:33:26 +0800

    rewrite webdav

--- webdav.go
+++ webdav.go
@@ -3,48 +3,55 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/webdav"
 )
 
 func initWebdav(mux *http.ServeMux) {
-	rootWebdav := &webdav.Handler{
-		FileSystem: webdav.Dir("/"),
-		LockSystem: webdav.NewMemLS(),
-	}
-	http.HandleFunc("/dav/", func(w http.ResponseWriter, r *http.Request) {
-		// fix got dir but got file
-		r.URL.Path = strings.TrimPrefix(r.URL.Path, "/dav")
-		rewriteURLIfDir(r)
-		if denyNotAllowedDir(w, r) {
+	fs := webdav.Dir("/")
+	lock := webdav.NewMemLS()
+
+	const prefix = "/dav/"
+	http.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
+
+		var hprefix = prefix
+
+		fpath := strings.TrimPrefix(r.URL.Path, "/dav")
+		deny := db.IsDeny(fpath)
+		if deny {
+			w.WriteHeader(403)
+			fmt.Fprintf(w, "the folder is deny access.")
 			return
 		}
-		rootWebdav.ServeHTTP(w, r)
+		// fix webdav dir list content include itself when prefix is not the real request path
+		realURI := r.Header.Get("Httprelay-Proxy-Url")
+		if realURI == "" {
+			realURI = r.Header.Get("X-Forwarded-Uri")
+		}
+		if realURI != "" {
+			uri, err := url.Parse(realURI)
+			if err != nil {
+				w.WriteHeader(400)
+				fmt.Fprintf(w, "real uri parse failed")
+				return
+			}
+			ss := strings.Split(uri.Path, prefix)
+			if len(ss) < 2 {
+				w.WriteHeader(400)
+				fmt.Fprintf(w, "real uri parse failed.2")
+				return
+			}
+			hprefix = ss[0] + prefix
+			r.URL.Path = ss[0] + r.URL.Path
+			fmt.Println(ss[0])
+		}
+		srv := &webdav.Handler{
+			FileSystem: fs,
+			LockSystem: lock,
+			Prefix:     hprefix,
+		}
+		srv.ServeHTTP(w, r)
 	})
-}
-
-func rewriteURLIfDir(r *http.Request) {
-	if strings.HasSuffix(r.URL.Path, "/") {
-		return
-	}
-	stat, err := os.Stat(r.URL.Path)
-	if err != nil {
-		return
-	}
-	if stat.IsDir() {
-		r.URL.Path += "/"
-		return
-	}
-}
-
-func denyNotAllowedDir(w http.ResponseWriter, r *http.Request) (done bool) {
-	deny := db.IsDeny(r.URL.Path)
-	if deny {
-		w.WriteHeader(403)
-		fmt.Fprintf(w, "the folder is deny access.")
-		return true
-	}
-	return
 }
