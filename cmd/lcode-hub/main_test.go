cba1b3933f27640557d36cad9a7bcb16cbad4775
shynome <shynome@gmail.com>
2023-01-13 12:14:03 +0800

    避免错误运行

--- cmd/lcode-hub/main_test.go
+++ cmd/lcode-hub/main_test.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestMain(m *testing.T) {
+	var running string
+	f.StringVar(&running, "test.run", "", "golang test")
+	f.Parse(os.Args[1:])
+	if running != "^TestMain$" {
+		return
+	}
 	// VERSION = "test"
 	main()
 }
