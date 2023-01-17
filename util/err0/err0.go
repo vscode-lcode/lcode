41df6f420cb7a0cf21d3801fd3f8c3562e807782
shynome <shynome@gmail.com>
2023-01-18 00:19:58 +0800

    无法做到错误捕捉就去掉错误捕捉

--- util/err0/err0.go
+++ util/err0/err0.go
@@ -1,14 +1,13 @@
 package err0
 
 import (
-	"github.com/lainio/err2"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func Record(err *error, span trace.Span) {
-	err2.Handle(err, func() {
+	if *err != nil {
 		span.SetStatus(codes.Error, (*err).Error())
 		span.RecordError(*err)
-	})
+	}
 }
